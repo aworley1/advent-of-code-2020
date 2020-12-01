2f413412af959b95c85c0d86d35d718132fc213c
Andrew Worley <andrew.worley@johnlewis.co.uk>
2020-12-01 17:52:55 +0000

    Very small performance improvement

--- src/main/kotlin/day1/Day1.kt
+++ src/main/kotlin/day1/Day1.kt
@@ -2,18 +2,18 @@ package day1
 
 import common.readPuzzleInput
 
-fun solvePuzzlePart1(input: List<String>): List<Int> {
+fun solvePuzzlePart1(input: List<String>): Int {
     val intInput = input.map { it.toInt() }
 
     return intInput.flatMap { outer ->
         intInput.map { inner ->
             inner to outer
         }
-    }.filter { it.first + it.second == 2020 }
-        .map { it.first * it.second }
+    }.first { it.first + it.second == 2020 }
+        .let { it.first * it.second }
 }
 
-fun solvePuzzlePart2(input: List<String>): List<Int> {
+fun solvePuzzlePart2(input: List<String>): Int {
     val intInput = input.map { it.toInt() }
 
     return intInput.flatMap { veryOuter ->
@@ -22,8 +22,8 @@ fun solvePuzzlePart2(input: List<String>): List<Int> {
                 Triple(veryInner, inner, veryOuter)
             }
         }
-    }.filter { it.first + it.second + it.third == 2020 }
-        .map { it.first * it.second * it.third }
+    }.first { it.first + it.second + it.third == 2020 }
+        .let { it.first * it.second * it.third }
 }
 
 fun main() {
