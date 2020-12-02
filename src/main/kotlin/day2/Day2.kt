8f5b9986103816df643240b66bfac78600f22478
Andrew Worley <andrew.worley@johnlewis.co.uk>
2020-12-02 13:16:26 +0000

    Small readability improvements

--- src/main/kotlin/day2/Day2.kt
+++ src/main/kotlin/day2/Day2.kt
@@ -3,25 +3,28 @@ package day2
 import common.readPuzzleInput
 
 fun solvePuzzlePart2(input: List<String>): Int {
-    return input.map { policy(it) to it.split(":")[1].trim() }
-        .count { isPasswordValid(policy = it.first, password = it.second) }
+    return input
+        .map { policy(it) to password(it) }
+        .count { (policy, password) -> policy.validate(password) }
 }
 
-private fun isPasswordValid(password: String, policy: Policy): Boolean {
-    return password.containsLetterAtPosition(policy.letter, policy.firstPosition) xor
-            password.containsLetterAtPosition(policy.letter, policy.secondPosition)
-}
+private fun password(input: String) = input.split(":")[1].trim()
 
-private fun String.containsLetterAtPosition(letter: Char, position: Int) = this[position - 1] == letter
+private fun String.hasCharAtPosition(letter: Char, position: Int) = this[position - 1] == letter
 
 private fun policy(input: String): Policy {
     val policyString = input.split(":").first()
     val range = policyString.split(" ").first().split("-")
     val letter = policyString[policyString.lastIndex]
-    return Policy(firstPosition = range[0].toInt(), secondPosition = range[1].toInt(), letter = letter)
+    return Policy(firstPosition = range[0].toInt(), secondPosition = range[1].toInt(), character = letter)
 }
 
-data class Policy(val letter: Char, val firstPosition: Int, val secondPosition: Int)
+data class Policy(val character: Char, val firstPosition: Int, val secondPosition: Int) {
+    fun validate(password: String): Boolean {
+        return password.hasCharAtPosition(character, firstPosition) xor
+                password.hasCharAtPosition(character, secondPosition)
+    }
+}
 
 fun main() {
     println(solvePuzzlePart2(readPuzzleInput("day2.txt")))
