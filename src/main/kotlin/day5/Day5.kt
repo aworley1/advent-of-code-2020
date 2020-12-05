a265cf80a36eb0f8d30f7b708be08333e038ae4d
Andrew Worley <andrew.worley@johnlewis.co.uk>
2020-12-05 11:57:44 +0000

    Day 5 Part 1

--- src/main/kotlin/day5/Day5.kt
+++ src/main/kotlin/day5/Day5.kt
@@ -1,11 +1,26 @@
 package day5
 
-fun findRow(rowInput: String): Int {
-    val startRange = (0..127).toList()
+import common.readPuzzleInput
 
-    return rowInput.fold(startRange) { acc, op ->
-        if (op == 'F') acc.take(acc.size / 2)
+fun part1(input: List<String>) = input.map { findSeatId(it) }.maxOrNull()
+
+fun findSeatId(input: String): Int {
+    val rowId = findRow(input.take(7))
+    val columnId = findColumn(input.takeLast(3))
+
+    return (rowId * 8) + columnId
+}
+
+fun findRow(rowInput: String): Int = binarySearch(rowInput, 'F', 0..127)
+fun findColumn(columnInput: String): Int = binarySearch(columnInput, 'L', 0..7)
+
+private fun binarySearch(input: String, lowerChar: Char, startRange: IntRange): Int {
+    return input.fold(startRange.toList()) { acc, op ->
+        if (op == lowerChar) acc.take(acc.size / 2)
         else acc.takeLast(acc.size / 2)
     }.single()
+}
 
+fun main() {
+    println(part1(readPuzzleInput("day5.txt")))
 }
--- src/test/kotlin/day5/Day5KtTest.kt
+++ src/test/kotlin/day5/Day5KtTest.kt
@@ -2,10 +2,6 @@ package day5
 
 import assertk.assertThat
 import assertk.assertions.isEqualTo
-import assertk.assertions.isFalse
-import assertk.assertions.isTrue
-import matchers.isFailure
-import matchers.isSuccess
 import org.junit.jupiter.api.Test
 
 internal class Day5KtTest {
@@ -15,5 +11,19 @@ internal class Day5KtTest {
         assertThat(findRow("BFFFBBF")).isEqualTo(70)
         assertThat(findRow("FFFBBBF")).isEqualTo(14)
         assertThat(findRow("BBFFBBF")).isEqualTo(102)
+    }
+
+    @Test
+    fun `should find column for examples`() {
+        assertThat(findColumn("RLR")).isEqualTo(5)
+        assertThat(findColumn("RRR")).isEqualTo(7)
+        assertThat(findColumn("RLL")).isEqualTo(4)
+    }
+
+    @Test
+    fun `should find seatId for examples`() {
+        assertThat(findSeatId("BFFFBBFRRR")).isEqualTo(567)
+        assertThat(findSeatId("FFFBBBFRRR")).isEqualTo(119)
+        assertThat(findSeatId("BBFFBBFRLL")).isEqualTo(820)
     }
 }
