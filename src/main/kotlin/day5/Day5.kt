0f2e700f14f4654c4da637253d17b98d29363cbb
Andrew Worley <andrew.worley@johnlewis.co.uk>
2020-12-05 12:22:13 +0000

    Day 5 Part 2 - horrible mutable version

--- src/main/kotlin/day5/Day5.kt
+++ src/main/kotlin/day5/Day5.kt
@@ -4,6 +4,18 @@ import common.readPuzzleInput
 
 fun part1(input: List<String>) = input.map { findSeatId(it) }.maxOrNull()
 
+fun part2(input: List<String>): Int {
+    val seats = input.map { findSeatId(it) }.sorted()
+    var previousSeat = seats.first() - 1
+    for (seat in seats) {
+        if (seat != previousSeat + 1) {
+            return seat - 1
+        }
+        previousSeat = seat
+    }
+    return 0
+}
+
 fun findSeatId(input: String): Int {
     val rowId = findRow(input.take(7))
     val columnId = findColumn(input.takeLast(3))
@@ -23,4 +35,5 @@ private fun binarySearch(input: String, lowerChar: Char, startRange: IntRange): 
 
 fun main() {
     println(part1(readPuzzleInput("day5.txt")))
+    println(part2(readPuzzleInput("day5.txt")))
 }
