e4df89bf036f5a9dc86d50dc70eb225b4dc3c189
Andrew Worley <andrew.worley@johnlewis.co.uk>
2020-12-10 22:14:39 +0000

    Day 10 Part 1

--- src/main/kotlin/day10/Day10.kt
+++ src/main/kotlin/day10/Day10.kt
@@ -1,5 +1,6 @@
 package day10
 
+import common.readPuzzleInput
 import java.lang.RuntimeException
 
 fun chainToOutput(chain: List<Int>): Int {
@@ -10,4 +11,15 @@ fun chainToOutput(chain: List<Int>): Int {
     if (differences.keys.filterNot { it == 3 || it == 1 }.isNotEmpty()) throw RuntimeException("Only differences of 1 and 3 allowed")
 
     return differences[3]!!.size * differences[1]!!.size
+}
+
+fun solvePart1(input: List<Int>): Int {
+    val device = input.maxOrNull()!! + 3
+
+    return chainToOutput((input + device + 0).sorted())
+}
+
+fun main() {
+    val input = readPuzzleInput("day10.txt").map { it.toInt() }
+    println(solvePart1(input))
 }
--- src/test/kotlin/day10/Day10KtTest.kt
+++ src/test/kotlin/day10/Day10KtTest.kt
@@ -8,11 +8,50 @@ internal class Day10KtTest {
     @Test
     fun `should convert chain of adapters to part 1 output`() {
         //3 differences of 1 and 2 differences of 3
-        val testChain = listOf(1,2,3,6,7,10)
+        val testChain = listOf(1, 2, 3, 6, 7, 10)
 
-        val exampleChain = listOf(0,1,4,5,6,7,10,11,12,15,16,19,22)
+        val exampleChain = listOf(0, 1, 4, 5, 6, 7, 10, 11, 12, 15, 16, 19, 22)
 
         assertThat(chainToOutput(testChain)).isEqualTo(6)
         assertThat(chainToOutput(exampleChain)).isEqualTo(35)
+    }
+
+    @Test
+    fun `should solve example part 1`() {
+        val input = listOf(
+            28,
+            33,
+            18,
+            42,
+            31,
+            14,
+            46,
+            20,
+            48,
+            47,
+            24,
+            23,
+            49,
+            45,
+            19,
+            38,
+            39,
+            11,
+            1,
+            32,
+            25,
+            35,
+            8,
+            17,
+            7,
+            9,
+            4,
+            2,
+            34,
+            10,
+            3
+        )
+
+        assertThat(solvePart1(input)).isEqualTo(220)
     }
 }
