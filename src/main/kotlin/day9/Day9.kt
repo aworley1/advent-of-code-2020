aaf941cd87bf3f23d3cd2b3b59119ac0a1a03136
Andrew Worley <andrew.worley@johnlewis.co.uk>
2020-12-10 08:55:37 +0000

    Small optimisation of day 9 part 2

--- src/main/kotlin/day9/Day9.kt
+++ src/main/kotlin/day9/Day9.kt
@@ -1,11 +1,16 @@
 package day9
 
 import common.readPuzzleInput
+import kotlin.system.measureTimeMillis
 
 fun main() {
     val part1Answer = solvePart1(readPuzzleInput("day9.txt").map { it.toLong() }, 25)
     println(part1Answer)
-    println(solvePart2(readPuzzleInput("day9.txt").map { it.toLong() }, part1Answer))
+    println(
+        measureTimeMillis {
+            println(solvePart2(readPuzzleInput("day9.txt").map { it.toLong() }, part1Answer))
+        }
+    )
 }
 
 fun numberCombinations(input: List<Long>): Set<Long> {
@@ -32,11 +37,12 @@ fun solvePart1(input: List<Long>, preambleSize: Int): Long {
 }
 
 fun solvePart2(input: List<Long>, target: Long): Long {
-    (2..100).flatMap {
-        input.windowed(it)
-    }.forEach {
-        if (it.sum() == target) return it.minOrNull()!! + it.maxOrNull()!!
-    }
+    (2..input.size).asSequence()
+        .flatMap {
+            input.windowed(it)
+        }.forEach {
+            if (it.sum() == target) return it.minOrNull()!! + it.maxOrNull()!!
+        }
 
     throw RuntimeException("Not found")
 }
