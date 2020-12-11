d87bb1a375ffb851ffc5a7adf346366a31d6d9b2
Andrew Worley <andrew.worley@johnlewis.co.uk>
2020-12-11 13:30:34 +0000

    Day 10 part 2 passes tests but doesn't perform!

--- src/main/kotlin/day10/Day10.kt
+++ src/main/kotlin/day10/Day10.kt
@@ -1,7 +1,7 @@
 package day10
 
 import common.readPuzzleInput
-import java.lang.RuntimeException
+import java.util.concurrent.atomic.AtomicLong
 
 fun chainToOutput(chain: List<Int>): Int {
     val differences = chain.windowed(2)
@@ -13,13 +13,39 @@ fun chainToOutput(chain: List<Int>): Int {
     return differences[3]!!.size * differences[1]!!.size
 }
 
+fun validNext(adapter: Int, adapters: List<Int>): List<Int> {
+    return adapters.filter { it > adapter && it <= adapter + 3 }
+}
+
+fun buildValidNextAdapters(adapters: List<Int>): Map<Int, List<Int>> {
+    return adapters.map { it to validNext(it, adapters) }.toMap()
+}
+
 fun solvePart1(input: List<Int>): Int {
     val device = input.maxOrNull()!! + 3
 
     return chainToOutput((input + device + 0).sorted())
 }
 
+fun solvePart2(adapters: List<Int>): Long {
+    val counter = AtomicLong(0)
+    val terminalAdapter = adapters.maxOrNull()!!
+    val validAdapters = buildValidNextAdapters(adapters + 0)
+
+    doSomething(0, validAdapters, terminalAdapter, counter)
+
+    return counter.get()
+}
+
+fun doSomething(adapter: Int, nextAdapters: Map<Int, List<Int>>, terminalAdapter: Int, counter: AtomicLong) {
+    if (adapter == terminalAdapter) counter.incrementAndGet()
+    nextAdapters[adapter]!!.forEach {
+        doSomething(it, nextAdapters, terminalAdapter, counter)
+    }
+}
+
 fun main() {
     val input = readPuzzleInput("day10.txt").map { it.toInt() }
     println(solvePart1(input))
+    println(solvePart2(input))
 }
--- src/test/kotlin/day10/Day10KtTest.kt
+++ src/test/kotlin/day10/Day10KtTest.kt
@@ -1,6 +1,7 @@
 package day10
 
 import assertk.assertThat
+import assertk.assertions.containsOnly
 import assertk.assertions.isEqualTo
 import org.junit.jupiter.api.Test
 
@@ -18,40 +19,74 @@ internal class Day10KtTest {
 
     @Test
     fun `should solve example part 1`() {
-        val input = listOf(
-            28,
-            33,
-            18,
-            42,
-            31,
-            14,
-            46,
-            20,
-            48,
-            47,
-            24,
-            23,
-            49,
-            45,
-            19,
-            38,
-            39,
-            11,
-            1,
-            32,
-            25,
-            35,
-            8,
-            17,
-            7,
-            9,
-            4,
-            2,
-            34,
-            10,
-            3
-        )
-
-        assertThat(solvePart1(input)).isEqualTo(220)
+        assertThat(solvePart1(secondExample)).isEqualTo(220)
     }
+
+    @Test
+    fun `should find adapters that are valid next`() {
+        val input = listOf(1, 2, 3, 4, 5, 6)
+
+        assertThat(validNext(1, input)).containsOnly(2, 3, 4)
+    }
+
+    @Test
+    fun `should build map of adapters and valid next adapters`() {
+        val input = listOf(1, 2, 3, 5, 6, 7, 10)
+        val result = buildValidNextAdapters(input)
+
+        assertThat(result.size).isEqualTo(input.size)
+        assertThat(result[1]!!).containsOnly(2, 3)
+        assertThat(result[2]!!).containsOnly(3, 5)
+        assertThat(result[3]!!).containsOnly(5, 6)
+        assertThat(result[5]!!).containsOnly(6, 7)
+        assertThat(result[6]!!).containsOnly(7)
+        assertThat(result[7]!!).containsOnly(10)
+    }
+
+    @Test
+    fun `should solve first part2 example`() {
+        val input = listOf(16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4)
+
+        assertThat(solvePart2(input)).isEqualTo(8)
+    }
+
+    @Test
+    fun `should solve second part2 example`() {
+        assertThat(solvePart2(secondExample)).isEqualTo(19208)
+    }
+
+    private val secondExample = listOf(
+        28,
+        33,
+        18,
+        42,
+        31,
+        14,
+        46,
+        20,
+        48,
+        47,
+        24,
+        23,
+        49,
+        45,
+        19,
+        38,
+        39,
+        11,
+        1,
+        32,
+        25,
+        35,
+        8,
+        17,
+        7,
+        9,
+        4,
+        2,
+        34,
+        10,
+        3
+    )
+
 }
