cfe78d358152f6d906ded9c7aa59131f2700f12f
Andrew Worley <andrew.worley@johnlewis.co.uk>
2020-12-11 21:08:39 +0000

    Day 11 Part 1

--- src/main/kotlin/day11/Day11.kt
+++ src/main/kotlin/day11/Day11.kt
@@ -1,5 +1,23 @@
 package day11
 
+import common.readPuzzleInput
+import day11.State.EMPTY
+import day11.State.FLOOR
+import day11.State.OCCUPIED
+
+fun main() {
+    println(solvePart1(readPuzzleInput("day11.txt")))
+}
+
+fun solvePart1(input: List<String>): Int {
+    var plan = parse(input)
+    while (true) {
+        val newPlan = plan.next()
+        if (plan == newPlan) return plan.count { it.state == OCCUPIED }
+        plan = newPlan
+    }
+}
+
 fun parse(input: List<String>): List<Space> {
     return input.flatMapIndexed { y, row ->
         row.mapIndexed { x, space ->
@@ -8,7 +26,35 @@ fun parse(input: List<String>): List<Space> {
     }
 }
 
-data class Space(val x: Int, val y: Int, val state: State)
+fun List<Space>.next(): List<Space> = this.map {
+    val neighbours = findNeighbours(it)
+    when (it.state) {
+        OCCUPIED -> if (neighbours.count { it.state == OCCUPIED } >= 4) it.switch() else it
+        EMPTY -> if (neighbours.none { it.state == OCCUPIED }) it.switch() else it
+        FLOOR -> it
+    }
+}
+
+fun List<Space>.findNeighbours(space: Space): List<Space> = this.filter {
+    it.x >= space.x - 1 && it.x <= space.x + 1 &&
+            it.y >= space.y - 1 && it.y <= space.y + 1
+}.filterNot { it.x == space.x && it.y == space.y }
+
+fun List<Space>.toDisplay(): List<String> = this.groupBy { it.y }
+    .toSortedMap()
+    .map {
+        it.value
+            .sortedBy { it.x }
+            .map { it.state.code }.joinToString("")
+    }
+
+data class Space(val x: Int, val y: Int, val state: State) {
+    fun switch() = when (state) {
+        EMPTY -> this.copy(state = OCCUPIED)
+        OCCUPIED -> this.copy(state = EMPTY)
+        FLOOR -> this
+    }
+}
 
 enum class State(val code: Char) {
     EMPTY('L'), OCCUPIED('#'), FLOOR('.');
--- src/test/kotlin/day11/Day11KtTest.kt
+++ src/test/kotlin/day11/Day11KtTest.kt
@@ -2,8 +2,10 @@ package day11
 
 import assertk.assertThat
 import assertk.assertions.containsOnly
-import day11.State.*
-import org.junit.jupiter.api.Assertions.*
+import assertk.assertions.isEqualTo
+import day11.State.EMPTY
+import day11.State.FLOOR
+import day11.State.OCCUPIED
 import org.junit.jupiter.api.Test
 
 internal class Day11KtTest {
@@ -22,5 +24,145 @@ internal class Day11KtTest {
             Space(1, 1, OCCUPIED),
             Space(2, 1, EMPTY),
         )
+    }
+
+    @Test
+    fun `should convert plan to strings`() {
+        val plan = listOf(
+            Space(2, 1, EMPTY),
+            Space(0, 0, OCCUPIED),
+            Space(0, 1, FLOOR),
+            Space(1, 0, FLOOR),
+            Space(1, 1, OCCUPIED),
+            Space(2, 0, EMPTY),
+        )
+
+        assertThat(plan.toDisplay()).isEqualTo(
+            listOf(
+                "#.L",
+                ".#L"
+            )
+        )
+    }
+
+    @Test
+    fun `should switch empty seat to occupied if no adjacent seats are occupied`() {
+        val input1 = parse(
+            listOf(
+                "LLL",
+                "LLL",
+                "LLL",
+            )
+        )
+
+        val input2 = parse(
+            listOf(
+                "...",
+                ".L.",
+                "...",
+            )
+        )
+
+        val result1 = input1.next()
+        val result2 = input2.next()
+
+        assertThat(result1.toDisplay()).isEqualTo(
+            listOf(
+                "###",
+                "###",
+                "###",
+            )
+        )
+
+        assertThat(result2.toDisplay()).isEqualTo(
+            listOf(
+                "...",
+                ".#.",
+                "...",
+            )
+        )
+    }
+
+    @Test
+    fun `should not become occupied if any neighbours are occupied`() {
+        val input = parse(
+            listOf(
+                "...",
+                ".L#",
+                "...",
+            )
+        )
+
+        val result = input.next().single { it.x == 1 && it.y == 1 }.state
+
+        assertThat(result).isEqualTo(EMPTY)
+    }
+
+    @Test
+    fun `should not become empty if fewer than 4 neighbours are occupied`() {
+        val input = parse(
+            listOf(
+                "L..",
+                ".##",
+                ".##",
+            )
+        )
+
+        val result = input.next().single { it.x == 1 && it.y == 1 }.state
+
+        assertThat(result).isEqualTo(OCCUPIED)
+    }
+
+    @Test
+    fun `should become empty if 4 or more neighbours are occupied`() {
+        val input = parse(
+            listOf(
+                "#..",
+                ".##",
+                ".##",
+            )
+        )
+
+        val result = input.next().single { it.x == 1 && it.y == 1 }.state
+
+        assertThat(result).isEqualTo(EMPTY)
+    }
+
+    @Test
+    fun `should find neighbours of a square`() {
+        val plan = parse(
+            listOf(
+                "...",
+                ".#.",
+                "...",
+            )
+        )
+
+
+        val neighbours = plan.findNeighbours(Space(1, 1, OCCUPIED))
+
+        assertThat(neighbours.map { it.x to it.y }).containsOnly(
+            0 to 0, 0 to 1, 0 to 2,
+            1 to 0, 1 to 2,
+            2 to 0, 2 to 1, 2 to 2
+        )
+    }
+
+    @Test
+    fun `should solve part 1 example`() {
+        val input = listOf(
+            "#.##.##.##",
+            "#######.##",
+            "#.#.#..#..",
+            "####.##.##",
+            "#.##.##.##",
+            "#.#####.##",
+            "..#.#.....",
+            "##########",
+            "#.######.#",
+            "#.#####.##",
+        )
+
+        assertThat(solvePart1(input)).isEqualTo(37)
     }
 }
