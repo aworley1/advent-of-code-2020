0fb819dd8356881e15e55ec644efc4440df65898
Andrew Worley <andrew.worley@johnlewis.co.uk>
2020-12-12 22:34:29 +0000

    Day 12 Part 2 doesn't work :(

--- src/main/kotlin/day12/Day12.kt
+++ src/main/kotlin/day12/Day12.kt
@@ -1,15 +1,20 @@
 package day12
 
 import common.readPuzzleInput
-import day12.Direction.*
+import day12.Direction.E
+import day12.Direction.N
+import day12.Direction.S
+import day12.Direction.W
+import day12.Rotate.*
 import kotlin.math.abs
 
 fun main() {
     println(solvePart1(readPuzzleInput("day12.txt")))
+    println(solvePart2(readPuzzleInput("day12.txt")))
 }
 
 fun solvePart1(input: List<String>): Int {
-    var boat = Boat(Coord(0,0), E)
+    var boat = Boat(Coord(0, 0), E)
 
     input.forEach {
         val regex = "([A-Z])([0-9]*)".toRegex()
@@ -29,12 +34,41 @@ fun solvePart1(input: List<String>): Int {
     return abs(boat.position.x) + abs(boat.position.y)
 }
 
+fun solvePart2(input: List<String>): Int {
+    var boatAndWaypoint = BoatAndWaypoint(
+        Boat(Coord(0, 0), E),
+        Boat(Coord(10, 1), E)
+    )
+
+    input.forEach {
+        val regex = "([A-Z])([0-9]*)".toRegex()
+        val (action, amountStr) = regex.matchEntire(it)!!.destructured
+        val amount = amountStr.toInt()
+        when (action) {
+            "N" -> boatAndWaypoint = boatAndWaypoint.moveWaypoint(N, amount)
+            "S" -> boatAndWaypoint = boatAndWaypoint.moveWaypoint(S, amount)
+            "E" -> boatAndWaypoint = boatAndWaypoint.moveWaypoint(E, amount)
+            "W" -> boatAndWaypoint = boatAndWaypoint.moveWaypoint(W, amount)
+            "F" -> boatAndWaypoint = boatAndWaypoint.moveBoatToWaypoint(amount)
+            "L" -> boatAndWaypoint = boatAndWaypoint.rotateWaypoint(amount, LEFT)
+            "R" -> boatAndWaypoint = boatAndWaypoint.rotateWaypoint(amount, RIGHT)
+        }
+    }
+
+    println(boatAndWaypoint)
+
+    return abs(boatAndWaypoint.boat.position.x) + abs(boatAndWaypoint.boat.position.y)
+}
+
 data class Coord(val x: Int, val y: Int) {
     operator fun plus(other: Coord): Coord =
         Coord(x + other.x, y + other.y)
 
     operator fun times(scale: Int): Coord =
         Coord(x * scale, y * scale)
+
+    operator fun minus(other: Coord) =
+        Coord(x - other.x, y - other.y)
 }
 
 data class Boat(val position: Coord, val direction: Direction) {
@@ -62,4 +96,52 @@ enum class Direction(val delta: Coord, val compassDegrees: Int) {
         val newCompassDegrees = (compassDegrees + degrees) % 360
         return values().single { it.compassDegrees == newCompassDegrees }
     }
+}
+
+enum class Rotate { LEFT, RIGHT }
+
+data class BoatAndWaypoint(
+    val boat: Boat,
+    val waypoint: Boat
+) {
+    fun moveBoatToWaypoint(number: Int): BoatAndWaypoint {
+        val newBoatPosition = (waypoint.position - boat.position) * number + boat.position
+        val newWaypointPosition = newBoatPosition + waypoint.position - boat.position
+        return BoatAndWaypoint(
+            boat = boat.copy(position = newBoatPosition),
+            waypoint = waypoint.copy(position = newWaypointPosition)
+        )
+    }
+
+    fun rotateWaypoint(degrees: Int, direction: Rotate) = this.copy(
+        waypoint = waypoint.copy(
+            position = rotateAboutOrigin(waypoint.position, boat.position, degrees, direction)
+        )
+    )
+
+    fun moveWaypoint(direction: Direction, amount: Int) =
+        this.copy(waypoint = waypoint.move(direction, amount))
+}
+
+private fun rotateAboutOrigin(coord: Coord, origin: Coord, degrees: Int, direction: Rotate): Coord {
+    val timesToRotate = when (direction) {
+        LEFT -> ((360 - degrees) / 90)
+        RIGHT -> degrees / 90
+    }
+
+    println(timesToRotate)
+
+    return (1..timesToRotate).fold(coord) { acc, _ -> rotate90RightAboutOrigin(acc, origin) }
+}
+
+private fun rotate90RightAboutOrigin(coord: Coord, origin: Coord): Coord {
+    val relativeToOrigin = coord - origin
+    val rotated = when {
+        relativeToOrigin.x > 0 && relativeToOrigin.y > 0 -> Coord(relativeToOrigin.x, -relativeToOrigin.y)
+        relativeToOrigin.x < 0 && relativeToOrigin.y < 0 -> Coord(relativeToOrigin.x, -relativeToOrigin.y)
+        relativeToOrigin.x > 0 && relativeToOrigin.y < 0 -> Coord(-relativeToOrigin.x, relativeToOrigin.y)
+        relativeToOrigin.x < 0 && relativeToOrigin.y > 0 -> Coord(-relativeToOrigin.x, relativeToOrigin.y)
+        else -> relativeToOrigin
+    }
+    return rotated + origin
 }
--- src/test/kotlin/day12/Day12KtTest.kt
+++ src/test/kotlin/day12/Day12KtTest.kt
@@ -6,62 +6,209 @@ import day12.Direction.E
 import day12.Direction.N
 import day12.Direction.S
 import day12.Direction.W
+import day12.Rotate.LEFT
+import day12.Rotate.RIGHT
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 internal class Day12KtTest {
-    @Test
-    fun `should move the boat in some directions`() {
-        val boat = Boat(Coord(0, 0), E)
+    @Nested
+    inner class Part1 {
+        @Test
+        fun `should move the boat in some directions`() {
+            val boat = Boat(Coord(0, 0), E)
 
-        assertThat(boat.move(E, 2)).isEqualTo(Boat(Coord(2, 0), E))
-        assertThat(boat.move(W, 2)).isEqualTo(Boat(Coord(-2, 0), E))
-        assertThat(boat.move(N, 2)).isEqualTo(Boat(Coord(0, 2), E))
-        assertThat(boat.move(S, 2)).isEqualTo(Boat(Coord(0, -2), E))
-    }
+            assertThat(boat.move(E, 2)).isEqualTo(Boat(Coord(2, 0), E))
+            assertThat(boat.move(W, 2)).isEqualTo(Boat(Coord(-2, 0), E))
+            assertThat(boat.move(N, 2)).isEqualTo(Boat(Coord(0, 2), E))
+            assertThat(boat.move(S, 2)).isEqualTo(Boat(Coord(0, -2), E))
+        }
 
-    @Test
-    fun `should move boat forward by some amount`() {
-        assertThat(Boat(Coord(0, 0), E).moveForwards(5))
-            .isEqualTo(Boat(Coord(5, 0), E))
+        @Test
+        fun `should move boat forward by some amount`() {
+            assertThat(Boat(Coord(0, 0), E).moveForwards(5))
+                .isEqualTo(Boat(Coord(5, 0), E))
 
-        assertThat(Boat(Coord(0, 0), N).moveForwards(3))
-            .isEqualTo(Boat(Coord(0, 3), N))
-    }
+            assertThat(Boat(Coord(0, 0), N).moveForwards(3))
+                .isEqualTo(Boat(Coord(0, 3), N))
+        }
 
-    @Test
-    fun `should rotate the boat right`() {
-        assertThat(Boat(Coord(0, 0), E).rotateRight(270).direction).isEqualTo(N)
-        assertThat(Boat(Coord(0, 0), N).rotateRight(90).direction).isEqualTo(E)
-        assertThat(Boat(Coord(0, 0), S).rotateRight(450).direction).isEqualTo(W)
-    }
+        @Test
+        fun `should rotate the boat right`() {
+            assertThat(Boat(Coord(0, 0), E).rotateRight(270).direction).isEqualTo(N)
+            assertThat(Boat(Coord(0, 0), N).rotateRight(90).direction).isEqualTo(E)
+            assertThat(Boat(Coord(0, 0), S).rotateRight(450).direction).isEqualTo(W)
+        }
 
-    @Test
-    fun `should rotate the boat left`() {
-        assertThat(Boat(Coord(0, 0), E).rotateLeft(270).direction).isEqualTo(S)
-        assertThat(Boat(Coord(0, 0), N).rotateLeft(90).direction).isEqualTo(W)
-        assertThat(Boat(Coord(0, 0), S).rotateLeft(450).direction).isEqualTo(E)
-    }
+        @Test
+        fun `should rotate the boat left`() {
+            assertThat(Boat(Coord(0, 0), E).rotateLeft(270).direction).isEqualTo(S)
+            assertThat(Boat(Coord(0, 0), N).rotateLeft(90).direction).isEqualTo(W)
+            assertThat(Boat(Coord(0, 0), S).rotateLeft(450).direction).isEqualTo(E)
+        }
 
-    @Test
-    fun `should solve part1 example`() {
-        val input = listOf(
-            "F10",
-            "N3",
-            "F7",
-            "R90",
-            "F11",
+        @Test
+        fun `should solve part1 example`() {
+            val input = listOf(
+                "F10",
+                "N3",
+                "F7",
+                "R90",
+                "F11",
             )
 
-        assertThat(solvePart1(input)).isEqualTo(25)
+            assertThat(solvePart1(input)).isEqualTo(25)
+        }
+
+        @Test
+        fun `should add coordinates`() {
+            assertThat(Coord(1, 1) + Coord(1, 3)).isEqualTo(Coord(2, 4))
+        }
+
+        @Test
+        fun `should scale coordinates`() {
+            assertThat(Coord(1, 1) * 3).isEqualTo(Coord(3, 3))
+        }
     }
 
-    @Test
-    fun `should add coordinates`() {
-        assertThat(Coord(1, 1) + Coord(1, 3)).isEqualTo(Coord(2, 4))
-    }
+    @Nested
+    inner class Part2 {
+        @Test
+        fun `should minus coords`() {
+            assertThat(Coord(1, 1) - Coord(1, 3)).isEqualTo(Coord(0, -2))
+        }
 
-    @Test
-    fun `should scale coordinates`() {
-        assertThat(Coord(1, 1) * 3).isEqualTo(Coord(3, 3))
+        @Test
+        fun `should move boat to waypoint twice`() {
+            val boatAndWaypoint = BoatAndWaypoint(
+                boat = Boat(Coord(10, 5), E),
+                waypoint = Boat(Coord(2, 2), E)
+            )
+
+            val result = boatAndWaypoint.moveBoatToWaypoint(2)
+
+            assertThat(result.boat.position).isEqualTo(Coord(-6, -1))
+            assertThat(result.waypoint.position).isEqualTo(Coord(-14, -4))
+
+        }
+
+        @Test
+        fun `should rotate waypoint 90 deg right around boat - quadrant 1`() {
+            val boatAndWaypoint = BoatAndWaypoint(
+                boat = Boat(Coord(1, 1), E),
+                waypoint = Boat(Coord(2, 2), E)
+            )
+
+            val result = boatAndWaypoint.rotateWaypoint(90, RIGHT)
+
+            assertThat(result.waypoint.position)
+                .isEqualTo(Coord(2, 0))
+        }
+
+        @Test
+        fun `should rotate waypoint 90 deg right around boat - quadrant 2`() {
+            val boatAndWaypoint = BoatAndWaypoint(
+                boat = Boat(Coord(1, 1), E),
+                waypoint = Boat(Coord(2, 0), E)
+            )
+
+            val result = boatAndWaypoint.rotateWaypoint(90, RIGHT)
+
+            assertThat(result.waypoint.position)
+                .isEqualTo(Coord(0, 0))
+        }
+
+        @Test
+        fun `should rotate waypoint 90 deg right around boat - quadrant 3`() {
+            val boatAndWaypoint = BoatAndWaypoint(
+                boat = Boat(Coord(1, 1), E),
+                waypoint = Boat(Coord(0, 0), E)
+            )
+
+            val result = boatAndWaypoint.rotateWaypoint(90, RIGHT)
+
+            assertThat(result.waypoint.position)
+                .isEqualTo(Coord(0, 2))
+        }
+
+        @Test
+        fun `should rotate waypoint 90 deg right around boat - quadrant 4`() {
+            val boatAndWaypoint = BoatAndWaypoint(
+                boat = Boat(Coord(1, 1), E),
+                waypoint = Boat(Coord(0, 2), E)
+            )
+
+            val result = boatAndWaypoint.rotateWaypoint(90, RIGHT)
+
+            assertThat(result.waypoint.position)
+                .isEqualTo(Coord(2, 2))
+        }
+
+        @Test
+        fun `should rotate waypoint 270 deg right around boat - quadrant 4`() {
+            val boatAndWaypoint = BoatAndWaypoint(
+                boat = Boat(Coord(1, 1), E),
+                waypoint = Boat(Coord(0, 2), E)
+            )
+
+            val result = boatAndWaypoint.rotateWaypoint(270, RIGHT)
+
+            assertThat(result.waypoint.position)
+                .isEqualTo(Coord(0, 0))
+        }
+
+        @Test
+        fun `should rotate waypoint 270 deg left around boat - quadrant 4`() {
+            val boatAndWaypoint = BoatAndWaypoint(
+                boat = Boat(Coord(1, 1), E),
+                waypoint = Boat(Coord(0, 0), E)
+            )
+
+            val result = boatAndWaypoint.rotateWaypoint(270, LEFT)
+
+            assertThat(result.waypoint.position)
+                .isEqualTo(Coord(0, 2))
+        }
+
+        @Test
+        fun `should rotate waypoint 90 deg left around boat - quadrant 4`() {
+            val boatAndWaypoint = BoatAndWaypoint(
+                boat = Boat(Coord(1, 1), E),
+                waypoint = Boat(Coord(0, 0), E)
+            )
+
+            val result = boatAndWaypoint.rotateWaypoint(90, LEFT)
+
+            assertThat(result.waypoint.position)
+                .isEqualTo(Coord(2, 0))
+        }
+
+        @Test
+        fun `should rotate waypoint 180 deg left around boat`() {
+            val boatAndWaypoint = BoatAndWaypoint(
+                boat = Boat(Coord(7, 5), E),
+                waypoint = Boat(Coord(10, 8), E)
+            )
+
+            val result = boatAndWaypoint.rotateWaypoint(180, LEFT)
+
+            assertThat(result.waypoint.position)
+                .isEqualTo(Coord(4, 2))
+        }
+
+        @Test
+        fun `should solve part2 example`() {
+            val input = listOf(
+                "F10",
+                "N3",
+                "F7",
+                "R90",
+                "F11",
+            )
+
+            val result = solvePart2(input)
+
+            assertThat(result).isEqualTo(286)
+        }
     }
 }
